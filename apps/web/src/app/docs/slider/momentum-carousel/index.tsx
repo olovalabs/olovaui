6cdc73b2da1db885ee90ae294beaccadae288d7d
seraprogrammer <programmernazmul1@gmail.com>
2026-06-15 19:43:15 +0600

    feat: added MomentumCarousel

---
+++ apps/web/src/app/docs/slider/momentum-carousel/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import MomentumCarousel from './momentum-carousel';
+
+export function MomentumCarouselView() {
+    return (
+        <div className='w-full'>
+            <MomentumCarousel />
+        </div>
+    );
+}
+
+export default MomentumCarouselView;
